9a2c96d072746ccb2fed5960165c8382b39e3469
SK <sarthakkarandikar03@gmail.com>
2024-08-29 18:33:14 +0530

    updated leetcode 31

--- nextPermutation.cpp
+++ nextPermutation.cpp
@@ -6,12 +6,12 @@ class Solution
     const int n = nums.size();
 
     int i;
-    for (i = n - 2; i >= 0; --i)
+    for (i = n - 2; i >= 0; i--)
       if (nums[i] < nums[i + 1])
         break;
 
     if (i >= 0)
-      for (int j = n - 1; j > i; --j)
+      for (int j = n - 1; j > i; j--)
         if (nums[j] > nums[i]) {
           swap(nums[i], nums[j]);
           break;
