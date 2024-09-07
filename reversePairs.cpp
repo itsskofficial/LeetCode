8bfc79167f78a118b1fa90310fc1a14c91404265
Sarthak Karandikar <sarthakkarandikar03@gmail.com>
2024-09-07 12:15:47 +0530

    added leetcode 493

---
+++ reversePairs.cpp
@@ -0,0 +1,70 @@
+class Solution 
+{
+    void merge(vector<int> &arr, int low, int mid, int high) 
+    {
+        vector<int> temp; 
+        int left = low;      
+        int right = mid + 1;   
+
+        while (left <= mid && right <= high) 
+        {
+            if (arr[left] <= arr[right]) {
+                temp.push_back(arr[left]);
+                left++;
+            }
+            else {
+                temp.push_back(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            temp.push_back(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp[i - low];
+        }
+    }
+
+    int mergeSort(vector<int> &arr, int low, int high) 
+    {
+        int count = 0;
+        if (low >= high) return 0;
+        int mid = (low + high) / 2 ;
+        count += mergeSort(arr, low, mid); 
+        count += mergeSort(arr, mid + 1, high);
+        count += countPairs(arr, low, mid, high);
+        merge(arr, low, mid, high);  
+        return count;
+    }
+
+    int countPairs(vector<int>& arr, int low, int mid, int high)
+    {
+        int right = mid + 1, count = 0;
+
+        for (int i = low; i <= mid; i++)
+        {
+            while(right <= high && (long long)arr[i] > 2LL * arr[right])
+            {
+                right++;
+            }
+
+            count += right - (mid + 1);
+        }
+
+        return count;
+    }
+
+    public:
+        int reversePairs(vector<int>& nums) 
+        {
+            return mergeSort(nums, 0, nums.size() - 1);
+        }
+};
