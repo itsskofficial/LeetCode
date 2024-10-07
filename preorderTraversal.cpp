bea73bdf7e551b1c0e60a0a4156ed1311b57914c
Sarthak Karandikar <itsskofficial03@gmail.com>
2024-10-07 09:11:10 +0530

    Update preorderTraversal.cpp

--- preorderTraversal.cpp
+++ preorderTraversal.cpp
@@ -48,12 +48,46 @@ class Solution
         }
     }
 
+    void preorderMorris(TreeNode* node, vector<int> &res)
+    {
+        TreeNode* curr = node;
+        while (curr != NULL)
+        {
+            if (curr -> left == NULL)
+            {
+                res.push_back(curr -> val);
+                curr = curr -> right;
+            }
+            else
+            {
+                TreeNode* temp = curr -> left;
+                while (temp -> right && temp -> right != curr)
+                {
+                    temp = temp -> right;
+                }
+                
+                if (temp -> right == NULL)
+                {
+                    temp -> right = curr;
+                    res.push_back(curr -> val);
+                    curr = curr -> left;
+                }
+                else
+                {
+                    temp -> right = NULL;
+                    curr = curr -> right;
+                }
+            }
+        }
+    }
+
+
     public:
         vector<int> preorderTraversal(TreeNode* root) 
         {
             vector<int> res;
 
-            preorderRecursive(root, res);
+            preorderMorris(root, res);
 
             return res;
         }
